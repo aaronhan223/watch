Fixed bugs in plotting (stderrs, window averaging)

src/main.py
@@ -347,7 +347,7 @@ if __name__ == "__main__":
     parser.add_argument('--plot_errors', type=bool, default=False, help='Whether to also plot absolute errors.')
     parser.add_argument('--schedule', type=str, default='variable', help='Training schedule: variable or fixed.')
     parser.add_argument('--n_seeds', type=int, default=1, help='Number of random seeds to run experiments on.')
-    parser.add_argument('--errs_window', type=int, default=100, help='Num observations to average for plotting errors.')
+    parser.add_argument('--errs_window', type=int, default=50, help='Num observations to average for plotting errors.')
     
     
     ## python main.py dataset muh_fun_name bias
@@ -387,11 +387,12 @@ if __name__ == "__main__":
         
         
         
-    paths_all.to_csv('../results/path_results.csv')
+    paths_all.to_csv(f'../results/path_results_{dataset0_name}_{muh_fun_name}_{dataset0_shift_type}shift_bias{cov_shift_bias}_nseeds{n_seeds}.csv')
     
     
     ## Compute average and stderr values for plotting
     paths_all_abs = paths_all.abs()
+    num_obs = paths_all_abs['obs_idx'].max()+1
     
     sigmas_0_means = []
     sigmas_1_means = []
@@ -402,19 +403,25 @@ if __name__ == "__main__":
     cs_abs_0_stderr = []
     cs_abs_1_stderr = []
     
+    ## For each fold/separate martingale path
     for i in range(0, 3):
         ## Compute average martingale values over trials
         sigmas_0_means.append(paths_all_abs[['sigmas_0_'+str(i), 'obs_idx']].groupby('obs_idx').mean())
         
-        ## Compute average (and stderr) absolute scores over trials
-        cs_mean_trials = paths_all_abs[['cs_0_'+str(i), 'obs_idx']].groupby('obs_idx').mean() 
-        cs_stderr_trials = paths_all_abs[['cs_0_'+str(i), 'obs_idx']].groupby('obs_idx').std() / n_seeds 
+        ## Compute average and stderr absolute score (residual) values over window, trials
+        cs_abs_0_means_fold = []
+        cs_abs_0_stderr_fold = []
+        for j in range(0, int(num_obs/errs_window)):
+            ## Subset dataframe by window
+            paths_all_abs_sub = paths_all_abs[paths_all_abs['obs_idx'].isin(np.arange(j*errs_window,(j+1)*errs_window))]
+            
+            ## Averages and stderrs for that window
+            cs_abs_0_means_fold.append(paths_all_abs_sub['cs_0_'+str(i)].mean())
+            cs_abs_0_stderr_fold.append(paths_all_abs_sub['cs_0_'+str(i)].std()/ np.sqrt(n_seeds*errs_window))
         
-        ## Average abs. scores over window
-        cs_abs_0_means.append([np.mean(np.abs(cs_mean_trials[(j*errs_window):((j+1)*errs_window)])) \
-                           for j in range(0, int(len(cs_mean_trials)/errs_window))]) ## cs averaged again over window
-        cs_abs_0_stderr.append([np.mean(np.abs(cs_stderr_trials[(j*errs_window):((j+1)*errs_window)])) \
-                           for j in range(0, int(len(cs_stderr_trials)/errs_window))]) ## cs averaged again over window
+        ## Averages and stderrs for that fold
+        cs_abs_0_means.append(cs_abs_0_means_fold)
+        cs_abs_0_stderr.append(cs_abs_0_stderr_fold)
         
         
     if (dataset1 is not None):
@@ -422,16 +429,20 @@ if __name__ == "__main__":
             ## Compute average martingale values over trials
             sigmas_1_means.append(paths_all_abs[['sigmas_1_'+str(i), 'obs_idx']].groupby('obs_idx').mean())
 
-            ## Compute average (and stderr) absolute scores over trials
-            cs_mean_trials = paths_all_abs[['cs_1_'+str(i), 'obs_idx']].groupby('obs_idx').mean() 
-            cs_stderr_trials = paths_all_abs[['cs_1_'+str(i), 'obs_idx']].groupby('obs_idx').std() / n_seeds 
+            ## Compute average and stderr absolute score (residual) values over window, trials
+            cs_abs_1_means_fold = []
+            cs_abs_1_stderr_fold = []
+            for j in range(0, int(num_obs/errs_window)):
+                ## Subset dataframe by window
+                paths_all_abs_sub = paths_all_abs[paths_all_abs['obs_idx'].isin(np.arange(j*errs_window,(j+1)*errs_window))]
 
-            ## Average abs. scores over window
-            cs_abs_1_means.append([np.mean(np.abs(cs_mean_trials[(j*errs_window):((j+1)*errs_window)])) \
-                               for j in range(0, int(len(cs_mean_trials)/errs_window))]) ## cs averaged again over window
-            cs_abs_1_stderr.append([np.mean(np.abs(cs_stderr_trials[(j*errs_window):((j+1)*errs_window)])) \
-                               for j in range(0, int(len(cs_stderr_trials)/errs_window))]) ## cs averaged again over window
-            
+                ## Averages and stderrs for that window
+                cs_abs_1_means_fold.append(paths_all_abs_sub['cs_1_'+str(i)].mean())
+                cs_abs_1_stderr_fold.append(paths_all_abs_sub['cs_1_'+str(i)].std()/ np.sqrt(n_seeds*errs_window))
+
+            ## Averages and stderrs for that fold
+            cs_abs_1_means.append(cs_abs_1_means_fold)
+            cs_abs_1_stderr.append(cs_abs_1_stderr_fold)
         
     
     plot_martingale_paths(
@@ -443,11 +454,12 @@ if __name__ == "__main__":
         cs_abs_0_stderr=cs_abs_0_stderr,
         cs_abs_1_stderr=cs_abs_1_stderr,
         errs_window=errs_window,
-        change_point_index=len(dataset0)*(1-test0_size)/3,
+        change_point_index= len(dataset0)*(1-test0_size)/3,
         title="Average paths of Shiryaev-Roberts Procedure",
         ylabel="Shiryaev-Roberts Statistics",
         file_name="shiryaev_roberts",
         dataset0_shift_type=dataset0_shift_type,
         cov_shift_bias=cov_shift_bias,
-        plot_errors=plot_errors
+        plot_errors=plot_errors,
+        n_seeds=n_seeds
     )

src/plot.py
@@ -5,7 +5,7 @@ import os
 def plot_martingale_paths(dataset0_paths, dataset0_name, dataset1_paths, cs_abs_0_means, cs_abs_1_means, cs_abs_0_stderr, \
                           cs_abs_1_stderr, errs_window=100, change_point_index=None,title="Martingale Paths", \
                           xlabel="Observation Index", ylabel="Simple Jumper Martingale Value", file_name="martingale_paths",\
-                          dataset0_shift_type = 'none',cov_shift_bias=0.0, plot_errors=False):
+                          dataset0_shift_type = 'none',cov_shift_bias=0.0, plot_errors=False, n_seeds=1):
     """
     Plot martingale paths for red wine and white wine groups over time, similar to Figure 2 in the paper.
     
@@ -42,7 +42,7 @@ def plot_martingale_paths(dataset0_paths, dataset0_name, dataset1_paths, cs_abs_
     plt.title(title, fontsize=26)
     
     if (dataset0_shift_type != 'none'):
-        plt.title(title + ', ' + dataset0_shift_type + ' shift, bias=' + str(cov_shift_bias), fontsize=22)
+        plt.title(f'{title}, {dataset0_shift_type} shift, {dataset0_shift_type} shift, \n bias={str(cov_shift_bias)}, n_seeds={n_seeds}', fontsize=20)
 #     plt.ylim([10.0, 100000])
     plt.legend(fontsize=15)
     plt.grid(True, which="both", ls="--")
@@ -56,16 +56,18 @@ def plot_martingale_paths(dataset0_paths, dataset0_name, dataset1_paths, cs_abs_
     if (plot_errors):
         plt.figure(figsize=(12, 8))
         
+        
         ### Plotting errors (ie, abs(scores))
         # Plot dataset0 group with dashed lines
         for i, cs in enumerate(cs_abs_0_means):
-            cs_averaged = [np.mean(np.abs(cs[(j*errs_window):((j+1)*errs_window)])) for j in range(0, len(cs))]
-            plt.plot(np.array(range(0, len(cs)))*errs_window, cs_averaged, label=dataset0_name + f' Fold {i+1}', linestyle='-', color=f'C{i}')
-            print(np.shape(cs-np.array(cs_abs_0_stderr[i])))
-            plt.fill_between(np.array(range(0, len(cs)))*errs_window, (cs-np.array(cs_abs_0_stderr[i])).flatten(), \
+            plt.plot(np.arange(0, len(cs)*errs_window, errs_window), cs, label=dataset0_name + f' Fold {i+1}', linestyle='-', color=f'C{i}')
+            
+            plt.fill_between(np.arange(0, len(cs)*errs_window, errs_window), (cs-np.array(cs_abs_0_stderr[i])).flatten(), \
                              (cs+np.array(cs_abs_0_stderr[i])).flatten(), alpha=0.5, color=f'C{i}')
+            
 
         # Plot dataset1 group with solid lines
+        
         for i, cs in enumerate(cs_abs_1_means):
             plt.plot(np.abs(cs), label=f'Red Wine Fold {i+1}', linestyle='-', color=f'C{i+3}')
 
@@ -74,7 +76,8 @@ def plot_martingale_paths(dataset0_paths, dataset0_name, dataset1_paths, cs_abs_
 
         plt.xlabel(xlabel, fontsize=24)
         plt.ylabel('Absolute error', fontsize=24)
-        plt.title('Error paths', fontsize=26)
+        plt.title(f'Error paths, {dataset0_shift_type} shift, {dataset0_shift_type} shift, \n bias={str(cov_shift_bias)}, n_seeds={n_seeds}', fontsize=20)
+
         plt.legend(fontsize=15)
         plt.grid(True, which="both", ls="--")
 